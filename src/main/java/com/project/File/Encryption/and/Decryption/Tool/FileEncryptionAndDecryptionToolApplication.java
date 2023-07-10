f522581e21393da4628f516ddeb14b674f3044d9
adeybabs <adeybabs.aa@gmail.com>
2023-07-10 15:11:09 +0100

    first commit

---
+++ src/main/java/com/project/File/Encryption/and/Decryption/Tool/FileEncryptionAndDecryptionToolApplication.java
@@ -0,0 +1,13 @@
+package com.project.File.Encryption.and.Decryption.Tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileEncryptionAndDecryptionToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileEncryptionAndDecryptionToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/File/Encryption/and/Decryption/Tool/FileEncryptionAndDecryptionToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.File.Encryption.and.Decryption.Tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileEncryptionAndDecryptionToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
